179f2f0d39860af2f98da5a70a180a559dc00aae
WReeves1141 <walter.reeves1141@gmail.com>
2024-07-12 16:04:23 -0400

    initial commit

--- key.py
+++ key.py
@@ -2,6 +2,7 @@
 import datetime as date
 import secrets
 from base64 import urlsafe_b64encode
+from math import floor
 
 
 class Key:
@@ -28,40 +29,30 @@ class Key:
 
     def to_json(self) -> dict[str, any]:
         """ Converts the key to .json file type. """
-        return {"k": str(self.as_base64url(), "utf-8")}
+
+        return {"k": str(self.as_base64url(), "utf-8"),
+                "activatesOn": floor(self.activates_on.timestamp()),
+                "deactivatesOn": floor(self.deactivates_on.timestamp()),
+                "expiresOn": floor(self.expires_on.timestamp())}
 
     def as_base64url(self) -> bytes:
         """ Converts the object to base 64. """
+
         return urlsafe_b64encode(self.bytes)
 
-    def status(self) -> bool or None:
-        """ Determines if key is activated, deactivated, or expired. """
-        if self.is_activated():
-            return True
-        elif self.is_deactivated():
-            return False
-        elif self.is_expired():
-            return None
-        else:
-            return self.activates_on
-
-    def is_activated(self):
+    def is_activated(self) -> bool:
         """ Checks to see if the key is activated. """
-        if self.activates_on > date.datetime.now():
-            return True
-        else:
-            return False
 
-    def is_deactivated(self):
+        return (self.activates_on <= date.datetime.now()
+                and not self.is_deactivated())
+
+    def is_deactivated(self) -> bool:
         """ Checks to see if the key is deactivated. """
-        if self.deactivates_on > date.datetime.now():
-            return True
-        else:
-            return False
 
-    def is_expired(self):
+        return (self.deactivates_on <= date.datetime.now()
+                and not self.is_expired())
+
+    def is_expired(self) -> bool:
         """ Checks to see if the key is expired. """
-        if self.expires_on > date.datetime.now():
-            return True
-        else:
-            return False
+
+        return self.expires_on <= date.datetime.now()
--- key_manager.py
+++ key_manager.py
@@ -1,33 +1,38 @@
 """ Programmer: Walter Reeves """
 from base64 import urlsafe_b64encode
 from key import Key
+import secrets
 
 
 class KeyManager:
-    """ ? """
+    """ Man the keys created in the Key class. """
     @staticmethod
-    def generate(num_keys) -> list:
-        """ Generates keys. """
-        keys = []
-        for num in range(1, num_keys + 1):
-            keys.append(Key.generate())
-        return keys
+    def generate(num_keys):
+        """ Generates the keys in a list. """
+
+        return KeyManager([Key.generate() for _ in range(1, num_keys + 1)])
+
+    def __init__(self, keys):
+        self.keys = keys
 
     def to_json(self) -> dict[str, any]:
         """ Converts the key to .json file type. """
-        key_dict = {}
-        Key.to_json()
 
-        return key_dict
+        return {"keys": [key.to_json() for key in self.keys]}
 
-    def get_encryption_key(self):
-        """ ? """
-        pass
+    def get_encryption_key(self) -> Key:
+        """ Provides a random key to the user. """
+
+        return secrets.choice(self.get_encryption_keys())
 
     def get_encryption_keys(self) -> tuple:
-        """ ? """
-        pass
+        """ Gets all possible encryption keys  """
+
+        return tuple(key for key in self.keys if key.is_activated()
+                     and not key.is_deactivated())
 
     def get_decryption_keys(self) -> tuple:
-        """ ? """
-        pass
+        """ Gets all active decryption keys. """
+
+        return tuple(key for key in self.keys if key.is_activated()
+                     or key.is_deactivated() and not key.is_expired())
