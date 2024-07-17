ef692899ff8b530d185ebc8761d233dcd89a42b0
Walter Reeves <walter.reeves1141@gmail.com>
2024-07-17 13:50:02 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-""" Programmer: Walter Reeves """
-# Testing Document
-from key_manager import KeyManager
-
-keys_to_generate = int(input("Enter the number of keys to generate > "))
-
-print(KeyManager.generate(num_keys=keys_to_generate).get_encryption_keys())
-
